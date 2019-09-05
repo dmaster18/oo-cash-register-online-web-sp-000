5132807929f54708c116bdb4c2436d1ae8881ac1
David Strauch <dmaster18@gmail.com>
2019-09-05 21:15:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   
   def initialize(total = 0, employee_discount = 0)
     @total = total
-    @employee_discount = employee_discount
+    @employee_discount = employee_discount.
     @item_names = []
     @items = {}
   end
