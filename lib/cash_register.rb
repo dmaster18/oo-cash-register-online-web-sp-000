9cca0fea2047c3497b403ef66f131466616ed6df
David Strauch <dmaster18@gmail.com>
2019-09-05 21:28:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     @item = item
     @item_names << @item
     i = 0 
-    if 
+    while i <= 
     @price = price
     @quantity = quantity
     @items[@item] = [@price, @quantity]
