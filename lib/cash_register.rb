9f532b54320ec4fd7110daa9ff202b4b1a87cd32
David Strauch <dmaster18@gmail.com>
2019-09-05 21:22:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_accessor :total
   
   def initialize(employee_discount = 0)
-    @total = total
+    @total = 0
     @employee_discount = employee_discount.to_i
     @item_names = []
     @items = {}
